f0d76a828ab3119fbbfee6f1f23669a8dc99eaea
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-09 17:20:15 +1000

    Added a function for plotting speckle patterns

---
+++ Week1_Trapped_Ions/src/plotting_utils.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+"""
+General figure setup
+"""
+
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=12)
+plt.rc('legend', fontsize=8)
+plt.rc('figure', facecolor='white')
+
+
+"""
+Plotting tools
+"""
+
+
+def speckle_pattern(vector, ax=None):
+
+    ax = ax if ax is not None else plt.gca()
+
+    color = 'red'
+    max_r = 0.45
+
+    ax.set_aspect('equal', 'box')
+
+    ax.set_facecolor('None')
+    ax.set_frame_on(False)
+
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+
+    for (x, y), r in np.ndenumerate(vector):
+        max_speckle = plt.Circle(
+            (x, 0), max_r, facecolor='None', edgecolor=color, linewidth=0.5
+        )
+        ax.add_patch(max_speckle)
+
+        speckle = plt.Circle(
+            (x, 0), np.sqrt(r) * max_r, facecolor=color, edgecolor=color
+        )
+        ax.add_patch(speckle)
+
+    ax.set_xlim([-0.55, len(vector)-1 + 0.55])
+    ax.set_ylim([-0.55, 0.55])
