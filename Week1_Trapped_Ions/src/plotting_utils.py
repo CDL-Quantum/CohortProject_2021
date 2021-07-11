8358c5190386ea7dd99dab1e192f2e645735769b
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-11 17:54:17 +1000

    Minor code clean-up

--- Week1_Trapped_Ions/src/plotting_utils.py
+++ Week1_Trapped_Ions/src/plotting_utils.py
@@ -19,31 +19,29 @@ Plotting tools
 
 
 def speckle_pattern(vector, ax=None):
-
+    """
+    Function for plotting a speckle pattern for a state probability vector.
+    """
     ax = ax if ax is not None else plt.gca()
-
+    # Color and size of speckles.
     color = 'red'
     max_r = 0.45
-
+    # Remove frame and ticks.
     ax.set_aspect('equal', 'box')
-
     ax.set_facecolor('None')
     ax.set_frame_on(False)
-
     ax.xaxis.set_major_locator(plt.NullLocator())
     ax.yaxis.set_major_locator(plt.NullLocator())
-
+    # Plot a speckle for each entry.
     for (x, y), r in np.ndenumerate(vector):
-        max_speckle = plt.Circle(
+        outline = plt.Circle(
             (x, 0), max_r, facecolor='None', edgecolor=color, linewidth=0.5
         )
-        ax.add_patch(max_speckle)
-
+        ax.add_patch(outline)
         speckle = plt.Circle(
             (x, 0), np.sqrt(r) * max_r, facecolor=color, edgecolor=color
         )
         ax.add_patch(speckle)
-
     ax.set_xlim([-0.55, len(vector)-1 + 0.55])
     ax.set_ylim([-0.55, 0.55])
 
@@ -51,7 +49,7 @@ def speckle_pattern(vector, ax=None):
 def cumulative(samples, x):
     """
     Simple function for computing the cumulative distribution of a set of samples at
-    values defined by x
+    values defined by x.
     """
     counts = []
     for xi in x:
--- Week1_Trapped_Ions/src/simulation_utils.py
+++ Week1_Trapped_Ions/src/simulation_utils.py
@@ -2,29 +2,67 @@ import numpy as np
 import tncontract as tn
 import qutip as qt
 
+
+"""
+Single-qubit gates
+"""
+
+
 def single_qubit_gate(theta, phi):
     """
-    Return a generic single qubit unitary gate
+    Return a single-qubit unitary gate with angles theta and phi.
     """
-    gate = np.array([[np.cos(theta / 2), -1j * np.exp(-1j * phi) * np.sin(theta / 2)],
-                     [-1j * np.exp(1j * phi) * np.sin(theta / 2), np.cos(theta / 2)]])
+    gate = np.array([
+        [np.cos(theta / 2), -1j * np.exp(-1j * phi) * np.sin(theta / 2)],
+        [-1j * np.exp(1j * phi) * np.sin(theta / 2), np.cos(theta / 2)]
+    ])
     return tn.Tensor(gate.reshape(2, 2, 1, 1), ['physout', 'physin', 'left', 'right'])
+
+
+def single_qubit_gate_layer(n_sites, angles):
+    """
+    Return an MPO with single-qubit gates along the chain.
+    Parameters
+        ----------
+        n_sites : int
+        angles: list
+            List of tuples containing the single qubit rotation angles. Each site
+            needs (\theta_i, \phi_i).
+    """
+    mpo = [single_qubit_gate(*site_angles) for site_angles in angles]
+    return tn.onedim.MatrixProductOperator(mpo)
+
+
+def random_single_qubit_gate_layer(n_sites):
+    """
+    Return an MPO with random-angled single-qubit gates along the chain.
+    Parameters
+        ----------
+        n_sites : int
+    """
+    angles = [(2 * np.pi * np.random.rand(), 2 * np.pi * np.random.rand()) for _ in range(n_sites)]
+    return single_qubit_gate_layer(n_sites, angles)
+
+
+"""
+Two-qubit gates
+"""
 
 
 def two_qubit_ms_gate(theta):
     """
-    Return a two qubit addressed molmer sorenson gate
+    Return a two-qubit Molmer-Sorenson gate with angle theta.
     """
     gate = qt.operations.molmer_sorensen(theta, 2).full()
 
-    # turn the gate into Tensor with physical and virtual bonds
+    # Turn the gate into Tensor with physical and virtual bonds.
     gate_tensor = tn.Tensor(gate.reshape(4, 4, 1, 1), ['physout', 'physin', 'left', 'right'])
 
-    # split physical bonds into single qubit sites
+    # Split physical bonds into single-qubit sites.
     gate_tensor.split_index('physout', (2, 2), ['physout_1', 'physout_2'])
     gate_tensor.split_index('physin', (2, 2), ['physin_1', 'physin_2'])
 
-    # SVD cut the two qubit gate into small 2-site MPO
+    # SVD cut the two-qubit gate into small 2-site MPO.
     U, V = tn.tensor_svd(gate_tensor, row_labels=['physout_1', 'physin_1', 'left'], absorb_singular_values='left')
     U.replace_label(['physout_1', 'physin_1', 'svd_in'], ['physout', 'physin', 'right'])
     V.replace_label(['physout_2', 'physin_2', 'svd_out'], ['physout', 'physin', 'left'])
@@ -33,7 +71,7 @@ def two_qubit_ms_gate(theta):
 
 def two_qubit_gate_layer(n_sites, thetas, left=0):
     """
-    Return an MPO with MS gates along the chain with angles.
+    Return an MPO with Molmer-Sorenson gates along the chain.
     Parameters
         ----------
         n_sites : int
@@ -55,20 +93,20 @@ def two_qubit_gate_layer(n_sites, thetas, left=0):
         if left == 0:
             return tn.onedim.MatrixProductOperator(mpo)
         else:
-            # pad identities on each end of the chain
+            # Pads identities on each end of the chain
             return tn.onedim.MatrixProductOperator([id_tensor] + mpo + [id_tensor])
     else:
         if left == 0:
-            # put an identity tensor at end qubit
+            # Put an identity tensor at end qubit
             return tn.onedim.MatrixProductOperator(mpo + [id_tensor])
         else:
-            # put and identity tensor on the first qubits
+            # Put an identity tensor on the first qubit.
             return tn.onedim.MatrixProductOperator([id_tensor] + mpo)
 
 
 def random_two_qubit_gate_layer(n_sites, left=0):
     """
-    Return an MPO with random angle MS gates along the chain.
+    Return an MPO with random-angled Molmer-Sorenson gates along the chain.
     Parameters
         ----------
         n_sites : int
@@ -77,16 +115,17 @@ def random_two_qubit_gate_layer(n_sites, left=0):
             qubit 0 (aligned left) or 1 (aligned right).
     """
     if n_sites % 2 == 0 and left == 1:
-        # this case we need to padded identites at the ends of the chain
+        # This is case where we need to pad identities at the ends of the chain.
         n_gates = int((n_sites - 1) / 2)
     else:
         n_gates = int(np.floor(n_sites / 2))
     thetas = 2 * np.pi * np.random.rand(n_gates)
     return two_qubit_gate_layer(n_sites, thetas, left=left)
 
+
 def random_two_qubit_gate_ladder(n_sites):
     """
-    Return a random 2-qubit ladder circuit in the form of two MPOs.
+    Return a random two-qubit ladder circuit in the form of two MPOs.
     Parameters
         ----------
         n_sites : int
@@ -97,43 +136,38 @@ def random_two_qubit_gate_ladder(n_sites):
     layer_2 = random_two_qubit_gate_layer(n_sites, left=1)
     return layer_1, layer_2
 
-def single_qubit_gate_layer(n_sites, angles):
-    """
-    Return an MPO with MS gates along the chain with angles.
-    Parameters
-        ----------
-        n_sites : int
-        thetas: list
-            list of tuples containing the single qubit rotation angles. Each site
-            needs (\theta_i, \phi_i)
-    """
-    mpo = [single_qubit_gate(*site_angles) for site_angles in angles]
-    return tn.onedim.MatrixProductOperator(mpo)
 
-def random_single_qubit_gate_layer(n_sites):
-    """
-    Return an MPO with random single qubit rotations along the chain.
-    Parameters
-        ----------
-        n_sites : int
-    """
-    angles = [(2 *np.pi * np.random.rand(), 2 *np.pi * np.random.rand()) for _ in range(n_sites)]
-    return single_qubit_gate_layer(n_sites, angles)
+"""
+Noise models
+"""
 
-# Some Noise models
 
-def depolarising_channel(pdep):
-    return qt.kraus_to_super([np.sqrt(1-3.*pdep/4.)*qt.qeye(2),
-                              np.sqrt(pdep/4.)*qt.sigmax(),
-                              np.sqrt(pdep/4.)*qt.sigmay(),
-                              np.sqrt(pdep/4.)*qt.sigmaz()])
+def depolarising_channel(p):
+    """
+    Return the superoperator for a depolarising channel.
+    """
+    return qt.kraus_to_super([
+        np.sqrt(1 - 3.*p/4.) * qt.qeye(2),
+        np.sqrt(p/4.) * qt.sigmax(),
+        np.sqrt(p/4.) * qt.sigmay(),
+        np.sqrt(p/4.) * qt.sigmaz()
+    ])
+
 
 def bit_flip_layer(p, n_sites):
     """
-    Return a circuit layer each site experiences a bitflip error with
-    probability p
+    Return a circuit layer where each site experiences a bit-flip error with
+    probability p.
     """
-    x_error = tn.Tensor(qt.sigmax().full().reshape(2, 2, 1, 1), ['physout', 'physin', 'left', 'right'])
-    id_tensor = tn.Tensor(np.eye(2).reshape(2, 2, 1, 1), ['physout', 'physin', 'left', 'right'])
-    return tn.onedim.MatrixProductOperator([x_error if error_site else id_tensor for error_site in np.random.binomial(1, p, n_sites)])
+    x_error = tn.Tensor(
+        qt.sigmax().full().reshape(2, 2, 1, 1),
+        ['physout', 'physin', 'left', 'right']
+    )
+    id_tensor = tn.Tensor(
+        np.eye(2).reshape(2, 2, 1, 1),
+        ['physout', 'physin', 'left', 'right']
+    )
+    return tn.onedim.MatrixProductOperator(
+        [x_error if error_site else id_tensor for error_site in np.random.binomial(1, p, n_sites)]
+    )
 
