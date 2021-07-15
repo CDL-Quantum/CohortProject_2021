b0df1f70e73180a5d5d24faaab6b495ffa53f908
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-16 02:39:49 +1000

    Started work on (simulated) quantum UDMIS problem

--- Week2_Rydberg_Atoms/src/udmis.py
+++ Week2_Rydberg_Atoms/src/udmis.py
@@ -1,22 +1,8 @@
 import numpy as np
+import qutip as qt
 
 
 class AbstractUDMIS():
-    def mc_step(self, T):
-        """
-        Performs a full update of the Rydberg model using the Metropolis-Hastings algorithm.
-        """
-        current_energy = self.energy()
-        for _ in range(self.num_vertices):
-            vertex = self.rand_vertex()
-            dE = self.energy_diff(vertex)
-            if (dE < 0) or (np.random.rand() < np.exp(-dE / T)):
-                current_energy += dE
-                self.occupations[vertex] ^= 1
-        return current_energy
-
-
-class ClassicalUDMIS(AbstractUDMIS):
     def __init__(self, u, graph):
         super().__init__()
         self.u, self.graph = u, graph
@@ -39,6 +25,14 @@ class ClassicalUDMIS(AbstractUDMIS):
                     edges[i, j], edges[j, i] = True, True
         return edges
 
+    def rand_vertex(self):
+        """
+        Selects a site in the graph at random.
+        """
+        return np.random.randint(self.num_vertices)
+
+
+class ClassicalUDMIS(AbstractUDMIS):
     def energy(self):
         """
         Returns the energy of the current Rydberg occupation configuration.
@@ -64,8 +58,72 @@ class ClassicalUDMIS(AbstractUDMIS):
             interaction_term_change = self.u * num_adjacent_occupied
         return interaction_term_change + onsite_term_change
 
-    def rand_vertex(self):
+    def mc_step(self, T):
         """
-        Selects a site in the graph at random.
+        Performs a full update of the Rydberg model using the Metropolis-Hastings algorithm.
         """
-        return np.random.randint(self.num_vertices)
+        current_energy = self.energy()
+        for _ in range(self.num_vertices):
+            vertex = self.rand_vertex()
+            dE = self.energy_diff(vertex)
+            if (dE < 0) or (np.random.rand() < np.exp(-dE / T)):
+                current_energy += dE
+                self.occupations[vertex] ^= 1
+        return current_energy
+
+
+class QuantumUDMIS(AbstractUDMIS):
+    def omega(self, t):
+        """
+        Calculate omega as a function of t.
+        """
+        if 0 <= t <= 0.25:
+            return (omega_max / 0.25) * t
+        elif 0.25 < t <= 0.69:
+            return omega_max
+        elif 0.69 < t <= 1:
+            return - omega_max * t / 0.31 + omega_max * (1 + 0.69 / 0.31)
+
+    def delta(self, t):
+        """
+        Calculate delta as a function of t.
+        """
+        slope = (delta_0 - delta_max) / (0.25 - 0.69)
+        if 0 <= t <= 0.25:
+            return delta_0
+        elif 0.25 < t <= 0.69:
+            return t * slope + (delta_max - slope * 0.69)
+        elif 0.69 < t <= 1:
+            return delta_max
+
+    def hamiltonian(self, t):
+        """
+        Returns the UD-MIS Hamiltonian.
+            H(t) = Ω(t) ∑_i σ_i^x - δ(t) ∑_i n_i + u ∑_ij n_i n_j
+        """
+        drive_term, onsite_term, interaction_term = 0, 0, 0
+        for i in range(self.num_vertices):
+            drive_term += identity_wrap(qt.sigmax(), i, self.num_vertices)
+            onsite_term += identity_wrap(qt.num(2), i, self.num_vertices)
+            for j in range(i + 1, self.num_vertices):
+                if self.edges[i, j]:
+                    interaction_term += \
+                        identity_wrap(qt.num(2), i, self.num_vertices) * identity_wrap(qt.num(2), j, self.num_vertices)
+        H = self.omega(t) * drive_term - self.delta(t) * onsite_term + self.u * interaction_term
+        return H
+
+    def time_evolution(self, dt):
+        """
+        Returns the time evolved state.
+        """
+
+
+"""
+Useful functions
+"""
+
+
+def identity_wrap(operator, i, n):
+    wrap = [qt.qeye(2) for _ in range(n)]
+    wrap[i] = operator
+    return qt.tensor(wrap)
