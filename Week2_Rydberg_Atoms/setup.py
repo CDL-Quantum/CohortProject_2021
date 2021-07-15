a5894afe858063b5d5a5f6477b0e64246f29aac2
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-15 15:46:06 +1000

    Added setup file, updated gitignore
    Added setup file, updated gitignore

---
+++ Week2_Rydberg_Atoms/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Week2_Rydberg_Atoms',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/opacity-quantum/CohortProject_2021/tree/main/Week2_Rydberg_Atoms',
+    license='',
+    author='Phil Evans',
+    author_email='phil@opacityquantum.com',
+    description='CDL - Week Two Project'
+)
