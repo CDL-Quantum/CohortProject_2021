28057857427809e9e08a60d702957ad01bac9ae1
sky-born <iskyfruit@gmail.com>
2021-07-15 20:38:05 +0900

    Task_3

--- Week2_Rydberg_Atoms/abstract_udmis.py
+++ Week2_Rydberg_Atoms/abstract_udmis.py
@@ -15,6 +15,20 @@ class AbstractUDMIS(abc.ABC):
     def rand_vertex(self):
         """Selects a site in the graph at random"""
     
+    # initial fucntion
+    # def mc_step(self, T):
+    #     """Performs a full update of the Rydberg model using the Metropolis-Hastings algorithm"""
+    #     current_energy = self.energy()
+    #     for _ in range(self.num_vertices):
+    #         vertex = self.rand_vertex()
+    #         dE = self.energy_diff(vertex)
+
+    #         if (dE < 0) or (np.random.rand() < np.exp(-dE / T)):
+    #             current_energy += dE
+    #             self.occupations[vertex] ^= 1
+
+    #     return current_energy
+
     def mc_step(self, T):
         """Performs a full update of the Rydberg model using the Metropolis-Hastings algorithm"""
         current_energy = self.energy()
