d136a3bab7a9639be987951a77a48b008487bbba
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-16 17:25:06 +1000

    Fixed classical_annealing figs, added code for quantum annealing

--- Week2_Rydberg_Atoms/src/figures.py
+++ Week2_Rydberg_Atoms/src/figures.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import qutip as qt
 
 """
 General figure setup
@@ -16,7 +17,7 @@ Plotting functions
 """
 
 
-def plot_annealing(ax, steps, energy, temperature):
+def plot_classical_annealing(ax, steps, energy, temperature):
     """
     Plot the energy and temperature as a function of annealing steps.
     """
@@ -24,33 +25,82 @@ def plot_annealing(ax, steps, energy, temperature):
     ax.plot(steps, energy, c='tab:blue')
     ax.tick_params(axis='y', labelcolor='tab:blue', color='tab:blue')
     ax.set_xlabel('Steps')
-    ax.set_ylabel(r'Energy (eV)', color='tab:blue')
+    ax.set_ylabel(r'Energy', color='tab:blue')
     ax.set_yticks(np.linspace(-3, 0, 4))
     # On the same axis, temperature as a function of steps (red).
     ax = ax.twinx()
     ax.plot(steps, temperature, c='tab:red')
     ax.tick_params(axis='y', labelcolor='tab:red', color='tab:red')
     ax.set_yticks(np.linspace(0, 100, 3))
-    ax.set_ylabel(r'Temperature (K)', color='tab:red')
+    ax.set_ylabel(r'Temperature', color='tab:red')
     # Recolor spines.
     ax.spines['left'].set_color('tab:blue')
     ax.spines['right'].set_color('tab:red')
 
 
-def plot_udmis(ax, udmis):
+def plot_classical_udmis(ax, udmis):
     """
     Plot the sites, connections and occupations for a given UD-MIS problem.
     """
-    # Draw the edges
+    # Draw the edges.
     for i in range(udmis.num_vertices):
         xi, yi = udmis.graph[i]
         for j in range(i + 1, udmis.num_vertices):
             xj, yj = udmis.graph[j]
             if udmis.edges[i, j]:
                 ax.plot([xi, xj], [yi, yj], 'k')
-    # Draw the sites
+    # Draw the sites.
     for i, (x, y) in enumerate(udmis.graph):
-        if udmis.occupations[i]:
+        if udmis.state[i]:
+            ax.plot(x, y, 'ko', mfc='k', ms=15)
+        else:
+            ax.plot(x, y, 'ko', mfc='w', ms=15)
+    # Adjust plotting area.
+    ax.set_xlim([0.05, 1.65])
+    ax.set_ylim([0.4, 3.65])
+    ax.set_xticks([0.5, 1.5])
+    ax.set_yticks([0.5, 1.5, 2.5, 3.5])
+    ax.set_aspect('equal', adjustable='box')
+
+
+def plot_quantum_annealing(ax, steps, energy, omega, delta):
+    """
+    Plot the energy and temperature as a function of annealing steps.
+    """
+    # Energy as a function of steps (blue).
+    ax.plot(steps, energy, c='tab:blue')
+    ax.tick_params(axis='y', labelcolor='tab:blue', color='tab:blue')
+    ax.set_xlabel('t')
+    ax.set_ylabel(r'$\langle \psi (t) | \hat H (t=1) | \psi (t) \rangle$', color='tab:blue')
+    ax.set_yticks(np.linspace(-3, 0, 4))
+    # On the same axis, temperature as a function of steps (red).
+    ax = ax.twinx()
+    ax.plot(steps, omega, c='tab:red', label=r'$\Omega(t)$')
+    ax.plot(steps, delta, c='tab:green', label=r'$\delta(t)$')
+    ax.set_yticks(np.linspace(-1, 2, 4))
+    ax.set_ylabel(r'$\Omega(t), \delta(t)$')
+    ax.legend()
+    # Recolor spines.
+    ax.spines['left'].set_color('tab:blue')
+
+
+def plot_quantum_udmis(ax, udmis):
+    """
+    Plot the sites, connections and occupations for a given UD-MIS problem.
+    """
+    # Draw the edges.
+    for i in range(udmis.num_vertices):
+        xi, yi = udmis.graph[i]
+        for j in range(i + 1, udmis.num_vertices):
+            xj, yj = udmis.graph[j]
+            if udmis.edges[i, j]:
+                ax.plot([xi, xj], [yi, yj], 'k')
+    # Draw the sites.
+    occupancies = qt.state_index_number(
+        udmis.state.dims[0], udmis.state.full().argmax()
+    )
+    for i, (x, y) in enumerate(udmis.graph):
+        if occupancies[i]:
             ax.plot(x, y, 'ko', mfc='k', ms=15)
         else:
             ax.plot(x, y, 'ko', mfc='w', ms=15)
--- Week2_Rydberg_Atoms/src/udmis.py
+++ Week2_Rydberg_Atoms/src/udmis.py
@@ -4,12 +4,11 @@ import qutip as qt
 
 class AbstractUDMIS():
     def __init__(self, u, graph):
-        super().__init__()
+        """
+        Find edges in the graph.
+        """
         self.u, self.graph = u, graph
         self.num_vertices = len(self.graph)
-
-        # Initialize system at infinite temperature (i.e. vertices are completely random and uncorrelated).
-        self.occupations = np.random.rand(self.num_vertices) < 0.5
         self.edges = self.find_edges()
 
     def find_edges(self):
@@ -17,7 +16,7 @@ class AbstractUDMIS():
         Calculate the distance between vertices; vertices within distance 1 share an edge.
         """
         edges = np.zeros((self.num_vertices, self.num_vertices), dtype=bool)
-        for i in range(self.num_vertices - 1):
+        for i in range(self.num_vertices-1):
             x_i, y_i = self.graph[i]
             for j in range(i+1, self.num_vertices):
                 x_j, y_j = self.graph[j]
@@ -33,32 +32,39 @@ class AbstractUDMIS():
 
 
 class ClassicalUDMIS(AbstractUDMIS):
+    def __init__(self, u, graph):
+        """
+        Initialize system with vertices randomly occupied.
+        """
+        super().__init__(u, graph)
+        self.state = np.random.rand(self.num_vertices) < 0.5
+
     def energy(self):
         """
         Returns the energy of the current Rydberg occupation configuration.
         """
         onsite_term, interaction_term = 0, 0
         for i in range(self.num_vertices):
-            onsite_term += self.occupations[i]
+            onsite_term += self.state[i]
             for j in range(i+1, self.num_vertices):
                 if self.edges[i, j]:
-                    interaction_term += self.occupations[i] * self.occupations[j]
-        return self.u * interaction_term - onsite_term
+                    interaction_term += self.state[i] * self.state[j]
+        return - onsite_term + self.u * interaction_term
 
     def energy_diff(self, i):
         """
-        Returns the energy difference resulting from flipping the occupation at the given coordinates.
+        Returns the energy difference caused by flipping the state at the given vertex.
         """
-        num_adjacent_occupied = sum(self.occupations[np.where(self.edges[i, :])[0]])
-        if self.occupations[i] == 1:
+        num_adjacent_occupied = sum(self.state[np.where(self.edges[i, :])[0]])
+        if self.state[i] == 1:
             onsite_term_change = 1.
             interaction_term_change = -self.u * num_adjacent_occupied
-        elif self.occupations[i] == 0:
+        elif self.state[i] == 0:
             onsite_term_change = -1.
             interaction_term_change = self.u * num_adjacent_occupied
-        return interaction_term_change + onsite_term_change
+        return onsite_term_change + interaction_term_change
 
-    def mc_step(self, T):
+    def monte_carlo_step(self, T):
         """
         Performs a full update of the Rydberg model using the Metropolis-Hastings algorithm.
         """
@@ -68,33 +74,41 @@ class ClassicalUDMIS(AbstractUDMIS):
             dE = self.energy_diff(vertex)
             if (dE < 0) or (np.random.rand() < np.exp(-dE / T)):
                 current_energy += dE
-                self.occupations[vertex] ^= 1
+                self.state[vertex] ^= 1
         return current_energy
 
 
 class QuantumUDMIS(AbstractUDMIS):
+    def __init__(self, u, graph, omega_max, delta_0, delta_max):
+        """
+        Initialize system with all qubits in the |0> state.
+        """
+        super().__init__(u, graph)
+        self.omega_max, self.delta_0, self.delta_max = omega_max, delta_0, delta_max
+        self.state = qt.tensor([qt.basis(2) for _ in range(self.num_vertices)])
+
     def omega(self, t):
         """
         Calculate omega as a function of t.
         """
         if 0 <= t <= 0.25:
-            return (omega_max / 0.25) * t
+            return (self.omega_max / 0.25) * t
         elif 0.25 < t <= 0.69:
-            return omega_max
+            return self.omega_max
         elif 0.69 < t <= 1:
-            return - omega_max * t / 0.31 + omega_max * (1 + 0.69 / 0.31)
+            return - self.omega_max * t / 0.31 + self.omega_max * (1 + 0.69 / 0.31)
 
     def delta(self, t):
         """
         Calculate delta as a function of t.
         """
-        slope = (delta_0 - delta_max) / (0.25 - 0.69)
+        slope = (self.delta_0 - self.delta_max) / (0.25 - 0.69)
         if 0 <= t <= 0.25:
-            return delta_0
+            return self.delta_0
         elif 0.25 < t <= 0.69:
-            return t * slope + (delta_max - slope * 0.69)
+            return t * slope + (self.delta_max - slope * 0.69)
         elif 0.69 < t <= 1:
-            return delta_max
+            return self.delta_max
 
     def hamiltonian(self, t):
         """
@@ -103,19 +117,25 @@ class QuantumUDMIS(AbstractUDMIS):
         """
         drive_term, onsite_term, interaction_term = 0, 0, 0
         for i in range(self.num_vertices):
-            drive_term += identity_wrap(qt.sigmax(), i, self.num_vertices)
-            onsite_term += identity_wrap(qt.num(2), i, self.num_vertices)
+            drive_term += identity_wrap(qt.sigmax(), self.num_vertices, i)
+            onsite_term += identity_wrap(qt.num(2), self.num_vertices, i)
             for j in range(i + 1, self.num_vertices):
                 if self.edges[i, j]:
-                    interaction_term += \
-                        identity_wrap(qt.num(2), i, self.num_vertices) * identity_wrap(qt.num(2), j, self.num_vertices)
-        H = self.omega(t) * drive_term - self.delta(t) * onsite_term + self.u * interaction_term
-        return H
+                    interaction_term += identity_wrap(qt.num(2), self.num_vertices, i) * \
+                                        identity_wrap(qt.num(2), self.num_vertices, j)
+        return self.omega(t) * drive_term - self.delta(t) * onsite_term + self.u * interaction_term
 
-    def time_evolution(self, dt):
+    def energy(self, t):
         """
-        Returns the time evolved state.
+        Returns the energy of the current Rydberg occupation configuration.
         """
+        return qt.expect(self.hamiltonian(t), self.state)
+
+    def time_evolution_step(self, t, dt):
+        """
+        Returns the time evolved quantum state.
+        """
+        return qt.sesolve(self.hamiltonian(t), self.state, [0, dt]).states[1]
 
 
 """
@@ -123,7 +143,7 @@ Useful functions
 """
 
 
-def identity_wrap(operator, i, n):
-    wrap = [qt.qeye(2) for _ in range(n)]
-    wrap[i] = operator
-    return qt.tensor(wrap)
+def identity_wrap(operator, num_sites, i):
+    operators = [qt.qeye(2) for _ in range(num_sites)]
+    operators[i] = operator
+    return qt.tensor(operators)
