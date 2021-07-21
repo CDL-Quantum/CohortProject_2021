7d21c45c9eaa6ca93e0b8eab4df7ffd28b4556d3
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-07-21 23:49:56 +1000

    Added setup files and gitignore

---
+++ Week3_VQE/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Week3_VQE',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/opacity-quantum/CohortProject_2021/tree/main/Week3_VQE',
+    license='',
+    author='Phil Evans',
+    author_email='phil@opacityquantum.com',
+    description='CDL - Week Three Project'
+)
