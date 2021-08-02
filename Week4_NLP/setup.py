3178bdc6e7c424952affebecaa53d7dbba665674
Tom Smith <thomas.b.smith@sydney.edu.au>
2021-08-02 14:01:14 +1000

    Initial setup

---
+++ Week4_NLP/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cdl-quantum-cohort-project-week-four',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/opacity-quantum/CohortProject_2021/tree/main/Week4_NLP',
+    license='',
+    author='Thomas Smith',
+    author_email='tom@opacityquantum.com',
+    description='CDL - Cohort Project: Week 4'
+)
